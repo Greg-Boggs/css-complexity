ba347b09c0629494236be343fda0b32525e83dce
Greg Boggs <gregboggs@153069.no-reply.drupal.org>
2014-05-30 16:50:48 -0700

    added some actual tests

--- scan.php
+++ scan.php
@@ -2,15 +2,33 @@
 layout: default
 ---
 <?php
+  include ("lib/functions.php");
   $target = strip_tags($_GET['url']);
   $email = strip_tags($_GET['email']);
+  $url = wash_url($target);
+
+  if (isset($url) && $url) {
+    $content = get_page($url);
+    if (empty($content) ) {
+      header('Location: /?error=content');
+    }
+  }
+  $matches = array(
+    'font-size' => substr_count($content, 'font-size'),
+    'color' => substr_count($content, 'color'),
+  );
+
 ?>
 <div class="wrap">
   <a href="/">New Scan</a> | <a href="/scan.php?url=<?php echo $target; ?>&email=<?php echo $email; ?>">Rescan</a>
   <fieldset class="results">
     <legend>Scan Results for <?php echo $url; ?></legend>
     <ol>
-      <li><?php echo "Hello World"; ?></li>
+      <?php foreach ($matches as $key => $match) { ?>
+        <li><?php echo "$key: $match"; ?></li>
+      <?php
+        }
+      ?>
     </ol>
   </fieldset>
 </div>
