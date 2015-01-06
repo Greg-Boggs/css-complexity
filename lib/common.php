930dcd3844b7f5ac21c41cd1f3d47095a68d69fb
Greg Boggs <gregboggs@153069.no-reply.drupal.org>
2015-01-06 10:29:18 -0800

    add agent constant

--- lib/common.php
+++ lib/common.php
@@ -1,3 +1,5 @@
 <?php
+  define('AGENT', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20041107 Firefox/1.0');
+
   //Generic patterns
   define('URL_MATCH', '|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i');
