12be5eb7368545749587a9a14c6c1d658b865f4c
Greg Boggs <me@gregboggs.com>
2015-02-21 13:50:18 -0800

    remove blog

--- index.php
+++ index.php
@@ -26,13 +26,4 @@ layout: default
             </div>
         </fieldset>
     </form>
-  <h1>Posts</h1>
-  <ul class="posts">
-    {% for post in site.posts %}
-      <li>
-        <span class="post-date">{{ post.date | date: "%b %-d, %Y" }}</span>
-        <a class="post-link" href="{{ post.url | prepend: site.baseurl }}">{{ post.title }}</a>
-      </li>
-    {% endfor %}
-  </ul>
 </div>
